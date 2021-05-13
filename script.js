686defc007dbf712cd48dd6c28d9b985cdd91132
Oscar <palomecoscar@gmail.com>
2021-05-12 22:42:25 -0500

    finished library

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-const button = document.getElementById("show-modal");
-const bookContainer = document.getElementById("book-container");
-
-
 let myLibrary = [
-  { title: "It", author: "Stephen King", pages: "1138", read: true },
+  /*{ title: "It", author: "Stephen King", pages: "1138", read: true },
   {
     title: "Big Little Lies",
     author: "Liane Moriarty",
@@ -11,31 +7,63 @@ let myLibrary = [
     read: false,
   },
   { title: "The analyst", author: "John Katzenbach", pages: "528", read: true },
-  { title: "Sharp Objects", author: "Gillian Flynn", pages: "328", read: true },
+  { title: "Sharp Objects", author: "Gillian Flynn", pages: "328", read: true }, */
 ];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+
+const modalButton = document.getElementById("show-modal");
+const bookContainer = document.getElementById("book-container");
+//const Title = document.getElementById("Title")
+//const title = document.querySelector("#title").value;
+
+
+
+
+class Book {
+  constructor(title, author, pages, read) {
+      this.title = title;
+      this.author = author;
+      this.pages = pages;
+      this.read = read;
+  }
+  toggleReadStatus() {
+      this.read = !this.read;
+      render();
+  }
 }
 
-function addBookToLibrary() {
-  let title = prompt("Name of the book: ");
-  let author = prompt("Who is the author?");
-  let pages = prompt("How many pages?");
-  let read = prompt("Have you read it?");
-  if (read == "true") {
-    read = true;
-  } else {
-    read = false;
-  }
+function addBookToLibrary(title, author, pages, read) {
   let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
-  displayBook(newBook);
-  console.log(myLibrary);
-  console.log(myLibrary.length);
+}
+
+//Function to delete books
+
+function deleteBook (btn){
+  myLibrary.splice(btn.id, 1);
+  render();
+  
+}
+
+//Function to add New Books
+
+const addBookBtn = document.querySelector("#add-book-button");
+
+addBookBtn.addEventListener('click', addNewBook);
+
+function addNewBook(e) {
+  e.preventDefault()
+
+  const title = document.getElementById("bookTitle");
+  const author = document.getElementById("bookAuthor");
+  const pages = document.getElementById("bookPages");
+  const read = document.getElementById("read");
+
+  if (validateSubmission(title, author, pages)) {
+    addBookToLibrary(title.value, author.value, pages.value, read.checked);
+    render();
+    clearForm(title, author, pages, read);
+  }
 }
 
 function showModal (){
@@ -46,8 +74,12 @@ function closeModal () {
   document.querySelector(".bg-modal").style.display = "none";
 }
 
-let displayBook = (book) => {
-    let div = document.createElement("div");
+modalButton.addEventListener("click", showModal);
+
+function render () {
+bookContainer.innerHTML = "";
+myLibrary.forEach((book, index) => {
+  let bookCard = document.createElement("div");
   let bookTitle = document.createElement("span");
   bookTitle.classList.add('text');
   let bookAuthor = document.createElement("span");
@@ -55,67 +87,61 @@ let displayBook = (book) => {
   let bookPages = document.createElement("span");
   bookPages.classList.add('text');
   let bookRead = document.createElement("span");
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "Delete"
   bookRead.classList.add('text');
   bookTitle.textContent = `Title: ${book.title}`;
   bookAuthor.textContent = `Author: ${book.author}`;
   bookPages.textContent = `Pages: ${book.pages}`;
-  bookRead.textContent = `Read: ${book.read}`;
-  div.style.cssText =
-    "box-sizing: border-box; width: 23%; background:#323232; display:flex; flex-direction:column; margin-left: 1%; margin-right:1%; border-radius:5%; align-items:center";
-  div.appendChild(bookTitle);
-  div.appendChild(bookAuthor);
-  div.appendChild(bookPages);
-  div.appendChild(bookRead);
-  bookContainer.appendChild(div);
-};
+  bookRead.style.cursor = "pointer"
+  if (myLibrary[index].read) {
+    bookRead.textContent = 'Read';
+    bookRead.style.color = '#2ba84a';
+} else {
+    bookRead.textContent = 'Unread';
+    bookRead.style.color = '#e83946';
+}
 
-button.addEventListener("click", showModal);
-
-console.log(myLibrary);
-
-myLibrary.forEach((book) => {
-  let div = document.createElement("div");
-  let bookTitle = document.createElement("span");
-  bookTitle.classList.add('text');
-  let bookAuthor = document.createElement("span");
-  bookAuthor.classList.add('text');
-  let bookPages = document.createElement("span");
-  bookPages.classList.add('text');
-  let bookRead = document.createElement("span");
-  bookRead.classList.add('text');
-  bookTitle.textContent = `Title: ${book.title}`;
-  bookAuthor.textContent = `Author: ${book.author}`;
-  bookPages.textContent = `Pages: ${book.pages}`;
-  bookRead.textContent = `Read: ${book.read}`;
-  div.style.cssText =
+bookRead.addEventListener('click', () => {
+  myLibrary[index].toggleReadStatus();
+  render();
+})
+  bookCard.style.cssText =
     "box-sizing: border-box; width: 23%; background:#323232; display:flex; flex-direction:column; margin-left: 1%; margin-right:1%; border-radius:5%; align-items:center; margin-bottom: 2rem";
-  div.appendChild(bookTitle);
-  div.appendChild(bookAuthor);
-  div.appendChild(bookPages);
-  div.appendChild(bookRead);
-  bookContainer.appendChild(div);
-  console.log(book.title);
+  bookCard.appendChild(bookTitle);
+  bookCard.appendChild(bookAuthor);
+  bookCard.appendChild(bookPages);
+  bookCard.appendChild(bookRead);
+  bookCard.appendChild(deleteButton);
+  bookContainer.appendChild(bookCard);
+
+  deleteButton.addEventListener('click', deleteBook);
+
+
 });
+}
 
 document.querySelector(".close").addEventListener("click", closeModal )
 
-/*function Book (title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        let status = "";
-        if (read == true){
-            status = "already read";
-        }else{
-            status = "not read yet";
-        }
 
-        return `${title} by ${author}, ${pages} pages, ${status}.`;
-
-    }
+//Check if fields are empty
+function validateSubmission (title, author, pages){
+  if(title.value == ""|| author.value == "" || pages.value ==""){
+    title.style.border = "1px solid red";
+    author.style.border = "1px solid red";
+    pages.style.border = "1px solid red";
+    return false;
+  } else {
+    
+    return true;
+  }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, false);
-console.log(theHobbit.info()); */
+//Function to clear fields
+
+function clearForm (title, author, pages, read) {
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    read.checked = false;
+}
